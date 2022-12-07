17c9565d2ac97918e0928bf268bcd72aa0e5ee35
Alexyeno <74510815+Alexyeno@users.noreply.github.com>
2022-12-07 22:09:58 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.set("strictQuery", true);
     await mongoose.connect(
-      "localhost:27017",
+      "mongodb+srv://localhost:27017",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
